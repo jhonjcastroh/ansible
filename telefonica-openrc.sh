13ce4189becc82658d3936ca2680194a91fb0901
Jhon <jhonjcastroh@gmail.com>
2019-03-27 15:10:06 -0300

    inventario dinamico para openstack y archivo de openrc de TelefonicaOpenCloud

---
+++ telefonica-openrc.sh
@@ -0,0 +1,8 @@
+export OS_USERNAME=nebulatsoft
+export OS_PASSWORD=nebula@2018
+export OS_TENANT_NAME=sa-chile-1
+export OS_AUTH_URL=https://ecs.sa-chile-1.telefonicaopencloud.com/v3/auth/tokens
+# The following lines can be omitted
+export OS_TENANT_ID=131d890deb5247d7a847128e90dca946
+export OS_REGION_NAME=sa-chile-1
+#export OS_CACERT=/path/to/cacertFile
